f1c2fa8aa636eec637e464a4c97e8aeba699069e
Zarathes <b.schut1@student.avans.nl>
2014-02-20 21:20:05 +0100

    DeelnemerActivity Alpha
    Basis hardcoded functies van het overzicht van deelnemers.
    Vereist verdere uitbreiding.

---
+++ src/avans/glassy/DeelnemerActivity.java
@@ -0,0 +1,43 @@
+package avans.glassy;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class DeelnemerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_deelnemer);
+		
+		GridView gridview = (GridView) findViewById(R.id.deelnemerGrid);
+	    gridview.setAdapter(new ImageAdapter(this));
+
+	    gridview.setOnItemClickListener(new OnItemClickListener() {
+	        public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+	            Toast.makeText(DeelnemerActivity.this, "" + position, Toast.LENGTH_SHORT).show();
+	        }
+	    });
+	    
+	   String deelnemers = String.valueOf(gridview.getAdapter().getCount());
+	   TextView deelnemerView =  (TextView) findViewById(R.id.deelnemerView);
+	   deelnemerView.setText(deelnemers);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
+
+
