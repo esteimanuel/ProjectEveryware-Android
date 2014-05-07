15bf4dd9fb5163ce54fce3eb71193cd27f4410ed
leonvantuyl <leonvtuyl@hotmail.com>
2014-05-07 13:08:10 +0200

    Maps Relatieve grootte,Staat nu op 50% van scherm

--- nl.avans.glassy/src/nl/avans/glassy/Views/WijkMapFragment.java
+++ nl.avans.glassy/src/nl/avans/glassy/Views/WijkMapFragment.java
@@ -2,9 +2,11 @@ package nl.avans.glassy.Views;
 
 
 import nl.avans.glassy.R;
+import android.app.ActionBar.LayoutParams;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.DisplayMetrics;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -28,6 +30,9 @@ public class WijkMapFragment extends Fragment  {
 		{
 			// Set WebView URL
 			webView = (WebView) rootView.findViewById(R.id.webview);
+			DisplayMetrics display = this.getResources().getDisplayMetrics();
+			final int height = display.heightPixels;
+			webView.getLayoutParams().height = (height/2);
 			webView.loadUrl(url);
 
 			// Prevent it from opening in a external browser
